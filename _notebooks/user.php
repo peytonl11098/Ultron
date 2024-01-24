dbc23d6199ba5a1005ed13c6ce6ec658c3168d82
peytonl11098 <ptleigh08@gmail.com>
2024-01-23 23:52:48 -0800

    userphp file

---
+++ _notebooks/user.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+  </body>
